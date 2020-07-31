57c9cd11ae371d2bd9a0270a32e7e239f813aec5
UG <ug@UGnoMacBook-Pro.local>
2020-07-31 11:44:59 +0900

    remove ignore functions

--- lib/blanton.ex
+++ lib/blanton.ex
@@ -1,18 +1,2 @@
 defmodule Blanton do
-  @moduledoc """
-  Documentation for `Blanton`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Blanton.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/blanton_test.exs
+++ test/blanton_test.exs
@@ -1,8 +1,3 @@
 defmodule BlantonTest do
   use ExUnit.Case
-  doctest Blanton
-
-  test "greets the world" do
-    assert Blanton.hello() == :world
-  end
 end
