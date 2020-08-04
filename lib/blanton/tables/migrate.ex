0b711324f7fb4dc448a066d2b655195908505970
UG <ug@UGnoMacBook-Pro.local>
2020-08-04 15:01:39 +0900

    bump 0.1.3

--- lib/blanton/tables/migrate.ex
+++ lib/blanton/tables/migrate.ex
@@ -11,15 +11,17 @@ defmodule Blanton.Tables.Migrate do
 
   :ok
   """
-  @spec run() :: :ok
-  def run() do
+  @spec run(String.t()) :: :ok
+  def run(path \\ "./lib/bq_schema") do
     verify_config!()
     load_all_deps()
 
     existing_tables = Blanton.Table.lists()
     IO.puts "Start migration!"
-    all_modules()
-    |> schema_modules()
+
+    {:ok, files} = File.ls(path)
+    files
+    |> to_schema(path)
     |> migrate_all!(existing_tables)
     IO.puts "All migration is succeeded!"
   end
@@ -55,28 +57,38 @@ defmodule Blanton.Tables.Migrate do
     IO.puts "--- #{module.__table_name__} updated. ---"
   end
 
-  @doc """
-  return bq_migration modules
-
-  ## example
-
-  Blanton.Tables.Migrate.schema_modules([
-    Blanton.Tables.Drop, Blanton.Tables.Migrate,
-    Blanton.Utils, Blanton.BqSchema.Users
-  ])
-  [Blanton.BqSchema.Users]
-  """
-  @spec schema_modules([atom()]) :: [atom()]
-  def schema_modules(all_modules) do
-    all_modules
-    |> Enum.map(fn mod -> Atom.to_string(mod) end)
-    |> Enum.filter(fn mod -> String.match?(mod, ~r/.BqSchema./) end)
-    |> Enum.map(fn mod -> String.to_atom(mod) end)
+  def to_schema(files, path) do
+    files
+    |> Enum.map(fn file ->
+      {module, _} = "#{path}/#{file}"
+      |> Code.require_file
+      |> List.first
+      module
+    end)
   end
 
-  @spec all_modules() :: [atom()]
-  defp all_modules() do
-    {:ok, modules} = :application.get_key(:blanton, :modules)
-    modules
-  end
+  # @doc """
+  # return bq_migration modules
+  #
+  # ## example
+  #
+  # Blanton.Tables.Migrate.schema_modules([
+  #   Blanton.Tables.Drop, Blanton.Tables.Migrate,
+  #   Blanton.Utils, Blanton.BqSchema.Users
+  # ])
+  # [Blanton.BqSchema.Users]
+  # """
+  # @spec schema_modules([atom()]) :: [atom()]
+  # def schema_modules(all_modules) do
+  #   all_modules
+  #   |> Enum.map(fn mod -> Atom.to_string(mod) end)
+  #   |> Enum.filter(fn mod -> String.match?(mod, ~r/.BqSchema./) end)
+  #   |> Enum.map(fn mod -> String.to_atom(mod) end)
+  # end
+  #
+  # @spec all_modules() :: [atom()]
+  # defp all_modules() do
+  #   {:ok, modules} = :application.get_key(:blanton, :modules)
+  #   modules
+  # end
 end
