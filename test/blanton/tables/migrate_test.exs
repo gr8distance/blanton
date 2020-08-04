6b1f42890bd8dc51a7cb2c9665c787faaae464c6
UG <ug@UGnoMacBook-Pro.local>
2020-08-04 14:59:06 +0900

    update test

--- test/blanton/tables/migrate_test.exs
+++ test/blanton/tables/migrate_test.exs
@@ -13,18 +13,4 @@ defmodule Blanton.Tables.MigrateTest do
   use ExUnit.Case
 
   doctest Blanton.Tables.Migrate
-
-  test "schema_modules(mods) return all schema modules" do
-    schema_modules = mods()
-    |> Blanton.Tables.Migrate.schema_modules()
-    assert schema_modules == [Blanton.BqSchema.Users]
-  end
-
-  defp mods() do
-    [
-      Blanton, Blanton.Column, Blanton.Connection, Blanton.Dataset, Blanton.Record,
-      Blanton.Schema, Blanton.Table, Blanton.Tables.Drop, Blanton.Tables.Migrate,
-      Blanton.Utils, Blanton.BqSchema.Users
-    ]
-  end
 end
