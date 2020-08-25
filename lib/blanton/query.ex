4412054936ed5c5d24b9cdaa9af30ab75b52e2ab
UG <ug@UGnoMacBook-Pro.local>
2020-08-25 18:32:11 +0900

    add docs

--- lib/blanton/query.ex
+++ lib/blanton/query.ex
@@ -2,11 +2,70 @@ defmodule Blanton.Query do
   alias Blanton.Query.{Select, From, Where, Order, Limit}
   require IEx
 
+  @moduledoc """
+  Build select query
+  """
+
+  @doc """
+  Build select query using dataset_id, table, conds
+
+  ## example
+
+  iex(1)> Blanton.Query.build("dataset", "table_name", where: [[:=, :name, "name"], [:in, :age, [20, 30]]], limit: 10)
+  %GoogleApi.BigQuery.V2.Model.QueryRequest{
+    connectionProperties: nil,
+    defaultDataset: nil,
+    dryRun: nil,
+    kind: nil,
+    labels: nil,
+    location: nil,
+    maxResults: nil,
+    maximumBytesBilled: nil,
+    parameterMode: nil,
+    preserveNulls: nil,
+    query: "SELECT * FROM dataset.table_name WHERE name = 'name' AND  age IN (20, 30) LIMIT 10",
+    queryParameters: nil,
+    requestId: nil,
+    timeoutMs: nil,
+    useLegacySql: nil,
+    useQueryCache: nil
+  }
+
+  You can pass conds
+  where, limit, order
+  """
+  @spec build(String.t(), String.t(), list()) :: GoogleApi.BigQuery.V2.Model.QueryRequest.t()
   def build(dataset_id, table, conds) do
     sql = generate_sql(dataset_id, table, conds)
     %GoogleApi.BigQuery.V2.Model.QueryRequest{query: sql}
   end
 
+  @doc """
+  Build select query using dataset_id, table, conds
+
+  ## example
+
+  iex(1)> Blanton.Query.build("SELECT * FROM dataset.users WHERE name = ?", ["gr8distance"])
+  %GoogleApi.BigQuery.V2.Model.QueryRequest{
+    connectionProperties: nil,
+    defaultDataset: nil,
+    dryRun: nil,
+    kind: nil,
+    labels: nil,
+    location: nil,
+    maxResults: nil,
+    maximumBytesBilled: nil,
+    parameterMode: nil,
+    preserveNulls: nil,
+    query: "SELECT * FROM dataset.users WHERE name = 'gr8distance'",
+    queryParameters: nil,
+    requestId: nil,
+    timeoutMs: nil,
+    useLegacySql: nil,
+    useQueryCache: nil
+  }
+  """
+  @spec build(String.t(), list()) :: GoogleApi.BigQuery.V2.Model.QueryRequest.t()
   def build(original, replace_values) when is_list(replace_values) do
     sql = original
     |> String.split("?")
@@ -17,6 +76,7 @@ defmodule Blanton.Query do
     %GoogleApi.BigQuery.V2.Model.QueryRequest{query: sql}
   end
 
+  @spec generate_sql(String.t(), String.t(), list()) :: String.t()
   defp generate_sql(dataset_id, table, conds) do
     [
       Select.clause(conds[:select]),
@@ -29,6 +89,8 @@ defmodule Blanton.Query do
     |> Enum.join(" ")
   end
 
+  @spec convert_replace(String.t()) :: String.s()
   defp convert_replace(value) when is_bitstring(value), do: "'#{value}'"
+
   defp convert_replace(value), do: value
 end
