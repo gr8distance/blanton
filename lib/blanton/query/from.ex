a65ee45c302704d4e1695352542dfb2433d957f7
UG <ug@UGnoMacBook-Pro.local>
2020-08-25 18:33:04 +0900

    add docs

--- lib/blanton/query/from.ex
+++ lib/blanton/query/from.ex
@@ -1,11 +1,9 @@
 defmodule Blanton.Query.From do
   import Blanton.Utils
 
-  def clause(dataset_id, table) do
-    "FROM #{dataset_id}.#{table}"
-  end
+  @spec clause(String.t(), String.t()) :: String.t()
+  def clause(dataset_id, table), do: "FROM #{dataset_id}.#{table}"
 
-  def clause(table) do
-    "FROM #{dataset_id()}.#{table}"
-  end
+  @spec clause(String.t()) :: String.t()
+  def clause(table), do: "FROM #{dataset_id()}.#{table}"
 end
