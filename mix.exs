4a0d4ce12d1b9030f4fae183b6b5563972e381a9
ug <gr8.distance@gmail.com>
2021-04-28 18:27:38 +0900

    update CI

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Blanton.MixProject do
   def project do
     [
       app: :blanton,
-      version: "0.1.5",
+      version: "0.2.0",
       elixir: "~> 1.10",
       description: "Blanton is a BigQuery library written by Elixir.",
       start_permanent: Mix.env() == :prod,
